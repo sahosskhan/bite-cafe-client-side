3aa098a429721b5b35af2bccd27446e899a2adbf
sahosskhan <sahosskhan359@gmail.com>
2024-04-17 04:50:00 +0600

    first basic file layout & base setup

---
+++ src/pages/conatact/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+        <div>
+          <h1>This Is Contact</h1>  
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/pages/menu/Menu.jsx
@@ -0,0 +1,11 @@
+
+
+const Menu = () => {
+    return (
+        <div>
+            <h1>This Is menu</h1>   
+        </div>
+    );
+};
+
+export default Menu;
