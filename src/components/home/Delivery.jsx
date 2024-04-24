9b75a432a030b6c9443bc713ef2629f5ba59580e
sahosskhan <sahosskhan359@gmail.com>
2024-04-24 21:57:25 +0600

    add footer

---
+++ src/components/home/Delivery.jsx
@@ -0,0 +1,13 @@
+
+
+const Delivery = () => {
+    return (
+        <>
+               <div className='bg-black my-16 h-44 flex items-center justify-center'>
+           <h1 className='font-serif font-semibold text-white lg:text-4xl text-2xl text-center'><i className="fa-solid fa-truck-fast"></i> For Home Delivery: +880 17780 30482</h1> 
+        </div>
+        </>
+    );
+};
+
+export default Delivery;
--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,10 +1,42 @@
 
 
 const Footer = () => {
+    const d = new Date();
+    const year = d.getFullYear();
     return (
-        <div>
-            <h1>footer</h1>
+        <>
+      <div className="">
+             <div className='bg-gray-900  text-white '>
+            <div className="grid grid-cols-1 md:grid-cols-2 text-white ">
+                <div className='flex justify-center items-center bg-gray-700 p-10'>
+                    <div className='w-auto space-y-1'>
+                        <h3 className='text-center text-2xl mb-2 font-medium'>Contact Us</h3>
+                        <p className='text-lg text-center'><i className="fa-solid fa-map-location-dot"></i> 20/1 Lawrence Lane, Thanapara, Kushtia</p>
+                        <p className='text-lg text-center'><i className="fa-solid fa-headset"></i> +88177803082, 778890</p>
+                        <p className='text-lg text-center'><i className="fa-solid fa-clock"></i> Mon - Fri: 08:00 - 22:00</p>
+                        <p className='text-lg text-center'><i className="fa-solid fa-clock"></i> Sat - Sun: 10:00 - 23:00</p>
+                    </div>
+                </div>
+                <div className='flex justify-center items-center p-10  bg-gray-800'>
+                    <div className='w-56'>
+                        <h3 className='text-center text-2xl mb-2 font-medium'>Follow Us</h3>
+                        <p className='text-lg text-center mb-4'>
+                            Join us on social media
+                        </p>
+                        <div className='flex justify-center gap-6 items-center   text-3xl'>
+                        <i className="fa-brands fa-square-facebook"></i>
+                        <i className="fa-brands fa-instagram"></i>
+                        <i className="fa-brands fa-youtube"></i>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <p className='flex justify-center text-xl py-4'>Copyright ©BiteCafe {year}. All rights reserved.</p>
         </div>
+        </div>
+        </>
     );
 };
 
