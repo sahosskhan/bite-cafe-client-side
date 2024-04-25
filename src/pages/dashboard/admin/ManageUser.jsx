314d4dce0e86e4ec6e59e993f6f5e0ca7e1339b7
sahosskhan <sahosskhan359@gmail.com>
2024-04-25 15:00:29 +0600

    add user delete

--- src/pages/dashboard/admin/ManageUser.jsx
+++ src/pages/dashboard/admin/ManageUser.jsx
@@ -1,14 +1,70 @@
+import Swal from "sweetalert2";
 import Loader from "../../../components/loader/Loader";
 import HeadingTitle from "../../../components/template/HeadingTitle";
+
 import useUserData from "../../../hooks/useUserData";
 
 
 const ManageUser = () => {
     const {userList, refetch, isLoading} = useUserData();
+
     
     if (isLoading) {
         return <Loader/>
     }
+
+    const handleMakeAdmin = item =>{
+        fetch(`http://localhost:5000/users/admin/${item._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${item.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+
+    const handleDelete = item => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/user-delete-one/${item._id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
+
+
+
+
     return (
         <div >
                         {
@@ -39,16 +95,20 @@ const ManageUser = () => {
         <tbody>
         {
                             userList?.map((item, index) =>
-            <tr  key={item._id} className="hover:bg-amber-50 hover:scale-110 scale-100 transition-all  border-b  duration-500">
+            <tr  key={item._id} className="hover:bg-amber-50  transition-all  border-b  duration-500">
                 <td className="py-4 px-6 border-b text-2xl font-medium">{index + 1}</td>
                 <td className="py-4 px-6 border-b text-2xl font-medium">{item.name}</td>
                 <td className="py-4 px-6 border-b text-2xl font-medium">{item.email}</td>
                 <td className="py-4 px-6 border-b text-2xl   font-medium">{item.role}</td>
                 <td className="py-4 px-2 border-b text-end">
-                    <button className="bg-green-500 hover:scale-110 scale-100 transition-all duration-500 text-white py-2 px-4 text-xl font-medium  rounded-md">Make Admin <i className="fa-solid fa-user-pen"></i></button>
-                </td>
-                <td className="py-4 px-2 border-b text-end">
-                    <button className="bg-red-500 hover:scale-110 scale-100 transition-all duration-500 text-white py-2 px-4 text-xl font-medium  rounded-md">Delete <i className="fa-solid fa-trash-can"></i></button>
+                    {
+                        item.role ==="admin"?      <button className="cursor-not-allowed bg-gray-500 hover:scale-110 scale-100 transition-all duration-500 text-white py-2 px-4 text-xl font-medium  rounded-md">Already Admin <i className="fa-solid fa-user-tie"></i></button> :    <button onClick={() => handleMakeAdmin(item)} className="bg-green-500 hover:scale-110 scale-100 transition-all duration-500 text-white py-2 px-4 text-xl font-medium  rounded-md">Make Admin <i className="fa-solid fa-user-pen"></i></button>
+                      
+
+                    }
+                    </td>
+                <td className="py-4 px-5 border-b text-end">
+                    <button onClick={() => handleDelete(item)} className="bg-red-500 hover:scale-110 scale-100 transition-all duration-500 text-white py-2 px-4 text-xl font-medium  rounded-md">Delete <i className="fa-solid fa-trash-can"></i></button>
                 </td>
             </tr>)
 }
