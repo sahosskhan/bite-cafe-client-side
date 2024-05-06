0e1a03719ff12376c82399ba8d01742a9f744e97
sahosskhan <sahosskhan359@gmail.com>
2024-05-06 12:12:07 +0600

    jwt verfication

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -2,13 +2,14 @@
 import { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
 import { app } from './firebase.config';
+import useAxiosPublic from './../hooks/useAxiosPublic';
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const gitprovider = new GithubAuthProvider();
 const fbprovider = new FacebookAuthProvider();
 const AuthProvider = ({ children }) => {
-
+ const axiosPublic = useAxiosPublic();
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -48,16 +49,29 @@ const AuthProvider = ({ children }) => {
         });
     }
     useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-        if (currentUser === null || currentUser.uid) {
+      const unsubscribe = onAuthStateChanged(auth, currentUser => {
           setUser(currentUser);
-        }
-        setLoading(false);
+          if (currentUser) {
+              const userInfo = {
+                email: currentUser.email,
+              };
+              axiosPublic.post("/jwt", userInfo).then((res) => {
+                if (res.data.token) {
+                  localStorage.setItem("access-token", res.data.token);
+                }
+              });
+            } else {
+              localStorage.removeItem("access-token");
+            }
+      
+          // console.log('current user', currentUser);
+          setLoading(false);
       });
       return () => {
-        unsubscribe();
-      };
-    }, []);
+          return unsubscribe();
+      }
+  }, [axiosPublic]);
+
 
     const authInfo = {
         user,
